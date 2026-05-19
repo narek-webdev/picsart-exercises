9ea9af12828557e9745724278c82b45dc4b5859a
Narek Sargsyan <73487750+narek-webdev@users.noreply.github.com>
2026-05-19 13:08:49 +0000

    commit

---
+++ buffered-i/o-task-1.c
@@ -0,0 +1,54 @@
+#include <stdio.h>
+#define FILE_NAME "data.txt"
+#define TEXT "Hello"
+#define BUFFER_SIZE 1024
+
+int main ()
+{
+FILE * stream = fopen(FILE_NAME, "w");
+
+if (stream == NULL) {
+ perror("Error");
+ return 1;
+}
+
+if (fputs(TEXT, stream) == EOF) {
+ perror("Error");
+ fclose(stream);
+ return 1;
+}
+
+if (fclose(stream) == EOF) {
+ perror("Error");
+ return 1;
+}
+
+stream = NULL;
+
+FILE * r_stream = fopen(FILE_NAME, "r");
+
+if (r_stream == NULL) {
+ perror("Error");
+ return 1;
+}
+
+char buff[BUFFER_SIZE];
+while(fgets(buff, sizeof(buff), r_stream) != NULL) {
+ printf("%s", buff);
+}
+
+if (ferror(r_stream)) {
+ perror("f_gets");
+ fclose(r_stream);
+ return 1;
+}
+
+if (fclose(r_stream) == EOF) {
+ perror("Error");
+ return 1;
+}
+
+r_stream = NULL;
+
+return 0;
+}
