c4489d0dd793115a632061107aafa4e77d702e5b
Narek Sargsyan <73487750+narek-webdev@users.noreply.github.com>
2026-05-21 11:31:29 +0000

    Add file processing functionality to count digits and write to output file

---
+++ practice-files/task-1.c
@@ -0,0 +1,56 @@
+#include <stdio.h>
+#define FILE_NAME "a.txt"
+
+int main ()
+{
+FILE * stream = fopen(FILE_NAME, "r");
+
+if (stream == NULL) {
+ perror("fopen");
+ return 1;
+}
+
+int count = 0;
+int ch;
+while ((ch = fgetc(stream)) != EOF)
+{
+ if (ch >= '0' && ch <= '9')
+ {
+  ++count;
+ }
+}
+
+if (ferror(stream))
+{
+ perror("fgetc");
+ fclose(stream);
+ return 1;
+}
+
+if (fclose(stream) == EOF)
+{
+ perror("fclose");
+ return 1;
+}
+
+FILE * bstream = fopen("b.txt", "w");
+
+if (bstream == NULL) {
+ perror("fopen");
+ return 1;
+}
+
+if (fprintf(bstream, "Your count is %i\n", count) < 0) {
+    perror("fprintf");
+    fclose(bstream);
+    return 1;
+} 
+
+if (fclose(bstream) == EOF)
+{
+ perror("fclose");
+ return 1;
+}
+
+return 0;
+}
